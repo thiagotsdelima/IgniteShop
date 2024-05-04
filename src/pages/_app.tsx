a333b32ba8b2073e93e06475d85150c593175800
thiago <thiagotsdelima@gmail.com>
2024-05-03 22:42:30 -0300

    update

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,16 @@
 import type { AppProps } from "next/app";
 import { globolStyles } from "../styles/global";
 import logoImg from '../assets/logo.svg'
+import { Container, Header } from '../styles/pages/app'
 
 globolStyles()
 export default function App({ Component, pageProps }: AppProps) {
   return (
-   <div>
-    <header>
+   <Container>
+    <Header>
       <img src={logoImg.src} alt="" />
-    </header>
+    </Header>
      <Component {...pageProps} />
-   </div>
+   </Container>
   )
 }
